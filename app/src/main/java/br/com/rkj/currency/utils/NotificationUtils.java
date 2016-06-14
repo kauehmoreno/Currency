f332ebfcc82b5153b92db9d5d749052bd474dd4b
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2016-06-14 01:27:13 -0300

    identecao

--- app/src/main/java/br/com/rkj/currency/utils/NotificationUtils.java
+++ app/src/main/java/br/com/rkj/currency/utils/NotificationUtils.java
@@ -36,7 +36,7 @@ public class NotificationUtils {
         }
 
         if(isAppInBackGround(context)){
-            int icon = R.mipmap.ic_launcher;
+            int icon = R.drawable.ic_launcher;
             Intent intent = new Intent(context, MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
             PendingIntent pendingIntent  = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
